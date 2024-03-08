cba757c61171ec131c4699390c47225312cea27d
unknown <aaron.hickman201@gmail.com>
2024-03-08 20:15:25 +0000

    extra css and day data added

--- src/components/DayData.js
+++ src/components/DayData.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function DayData({ day }) {
   return (
     <div className="data-container">
-      <div>{day}</div>
+      <div className="day-name-data-container">
+        <div className="day-of-week-data">{day}</div>
+      </div>
+      <div className="emo-temp-data-container">
+        <div className="emoji-data">&#x1F31E; </div>
+        <div className="temperature-data">22</div>
+      </div>
     </div>
   );
 }
