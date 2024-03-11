e27e1bb7122d01b006e41a9d5fb9668e374c5efb
unknown <aaron.hickman201@gmail.com>
2024-03-11 16:43:31 +0000

    icons showing

--- src/components/DayData.js
+++ src/components/DayData.js
@@ -1,20 +1,45 @@
 import React from "react";
 
 export default function DayData({ day, weatherData, fourDays }) {
+  // if weather data is empty, show loading div
+  const daysDataList = weatherData.list.slice(0, 4);
+
   if (Object.keys(weatherData).length === 0) {
     return <div>Loading...</div>;
   }
 
-  console.log(day);
+  const findIndex = () => {
+    let curIndex = 0;
+    for (let i = 0; i < fourDays.length; i++) {
+      if (fourDays[i] === day) {
+        curIndex = i;
+      }
+    }
+    return curIndex;
+  };
 
+  let curIndex = findIndex();
+
+  const currentDayData = daysDataList[curIndex];
+
+  // console.log(currentDayData);
+  // console.log(currentDayData.weather[0].icon);
   return (
     <div className="data-container">
       <div className="day-name-data-container">
         <div className="day-of-week-data">{day}</div>
       </div>
       <div className="emo-temp-data-container">
-        <div className="emoji-data">&#x1F31E; </div>
-        <div className="temperature-data">22</div>
+        <div className="emoji-data">
+          {
+            <img
+              src={`https://openweathermap.org/img/wn/${currentDayData.weather[0].icon}@2x.png`}
+            ></img>
+          }
+        </div>
+        <div className="temperature-data">
+          {`${Math.floor(currentDayData.main.temp)}\u00B0`}
+        </div>
       </div>
     </div>
   );
--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -2,6 +2,7 @@ import React from "react";
 import DayData from "./DayData";
 
 export default function WeatherData({ fourDays, weatherData }) {
+  console.log(weatherData);
   if (Object.keys(weatherData).length === 0) {
     return <div>Loading...</div>;
   }
@@ -9,12 +10,24 @@ export default function WeatherData({ fourDays, weatherData }) {
   return (
     <div className="weather-data-container">
       <div className="city-name-container">
-        <div>{weatherData.city.name}</div>
+        <div>
+          <div>{weatherData.city.name}</div>
+          <div className="weather-icon-container">
+            <img
+              src={`https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`}
+            ></img>
+          </div>
+        </div>
       </div>
 
       <div className="day-data-component">
         {fourDays.map((day) => (
-          <DayData day={day} key={day} weatherData={weatherData} />
+          <DayData
+            day={day}
+            key={day}
+            weatherData={weatherData}
+            fourDays={fourDays}
+          />
         ))}
       </div>
     </div>
